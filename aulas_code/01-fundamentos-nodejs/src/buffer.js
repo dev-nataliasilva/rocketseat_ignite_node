9ff365c29f391def417ed1e5e3c57e0e7cfc72c3
Natalia Silva <natalia.nsdeveloper@gmail.com>
2025-04-28 19:47:47 -0300

    fundamentos aula 13

---
+++ aulas_code/01-fundamentos-nodejs/src/buffer.js
@@ -0,0 +1,9 @@
+//buffer = representação de um espaço na memória de um computador,
+//usado principalmente para transitar dados de uma maneira muito rápida
+
+//TODO:pesquisar TypedArray por curiosidade
+
+const buf = Buffer.from('ok')
+console.log(buf) //hexadecimal
+console.log(buf.toJSON()) //decimal
+
