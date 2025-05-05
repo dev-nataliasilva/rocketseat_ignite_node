6c814e41e7bc3140b69e779093e0e1cc60f12e3e
Natalia Silva <natalia.nsdeveloper@gmail.com>
2025-05-05 16:43:28 -0300

    fundamentos aula 19

--- aulas_code/01-fundamentos-nodejs/src/routes.js
+++ aulas_code/01-fundamentos-nodejs/src/routes.js
@@ -29,5 +29,12 @@ export const routes = [
     
             return res.writeHead(201).end('Criação de usuários')
        }
+    },
+    {
+        method: 'DELETE',
+        path: '/users',
+        handler: (req, res) => {
+
+        }
     }
 ]
--- aulas_code/01-fundamentos-nodejs/src/server.js
+++ aulas_code/01-fundamentos-nodejs/src/server.js
@@ -2,6 +2,16 @@ import http from 'node:http'
 import { json } from './middlewares/json.js'//Type module no package.json exige que eu especifique o tipo de arquivo
 import { routes } from './routes.js'
 
+// Query Parameters > URL Stateful enviar informações que não são sensíveis, que servem mais para modificar a resposta do backend [FILTROS, PAGINAÇÃO, ...]
+// http://localhost:3333/users?userId=1&name=Diego     
+
+// Route Parameters > Identificação de recurso
+// GET http://localhost:3333/users/1
+// DELETE http://localhost:3333/users/1
+
+// Request Body > Envio de informações de um formulário (HTTPs). Diferente dos outros, NÃO ficam na URL.
+// POST http://localhost:3333/users  
+
 const server = http.createServer(async (request, response) => {
     const { method, url } = request
     
