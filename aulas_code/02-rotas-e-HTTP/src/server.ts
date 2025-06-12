120f9b9d5e9c496f22ed6e7937660e13d2402dd9
Natalia Silva <natalia.nsdeveloper@gmail.com>
2025-06-11 21:53:35 -0300

    rotas e http aula 02

---
+++ aulas_code/02-rotas-e-HTTP/src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+    return 'Hello World'
+})
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('HTTP Server Running!')
+})
+
+//npx tsx src/server.ts
+//npm run dev
